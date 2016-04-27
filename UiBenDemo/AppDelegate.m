163c7f5aecd76c650344fad6c92edb5b622db4e2
longfeima <1984796126@qq.com>
2016-04-27 10:17:22 +0800

    no message

--- UiBenDemo/AppDelegate.m
+++ UiBenDemo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window setRootViewController:[[UINavigationController alloc]
+                                        initWithRootViewController:[ViewController new]]];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+
     return YES;
 }
 
